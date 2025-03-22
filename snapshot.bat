741417ee9890355d0ab803b4ad9db654a6c82f04
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-03-22 18:56:55 +0800

    移除完全无用的文件

--- snapshot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo 正在创建版本指纹...
-git init > nul 2>&1
-git add . > nul 2>&1
-git commit -m "当前状态" > nul 2>&1
-git bundle create project.bundle HEAD > nul 2>&1
-echo 请将生成的 project.bundle 文件发给我
-echo 文件大小: %~z0
-pause
