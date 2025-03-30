18e55969c239d67e70909d9e1c6fa58e0bc05f9a
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-03-31 01:18:30 +0800

    只使用.rc就可以一键打包了

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --onefile --windowed --icon=logo.ico --add-data "icons/*;icons" --version-file=version_info.txt --name "IPTV扫描编辑工具" --noconfirm --clean main.py
+pyinstaller --onefile --windowed --icon=logo.ico --add-data "icons/*;icons" --version-file=version_info.rc --name "IPTV扫描编辑工具" --noconfirm --clean main.py
 pause
