66e34ca2ddf7c30eacec66565e912d57e9bd6598
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-03-31 22:20:13 +0800

    更新一键打包 包含vlc和ffmpeg

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --onefile --windowed --icon=logo.ico --add-data "icons/*;icons" --version-file=version_info.rc --name "IPTV扫描编辑工具" --noconfirm --clean main.py
+pyinstaller --onefile --windowed --icon=logo.ico --add-data "icons/*;icons" --add-data "ffmpeg/*;ffmpeg" --add-data "vlc/*;vlc" --version-file=version_info.rc --name "IPTV扫描编辑工具" --noconfirm --clean main.py
 pause
