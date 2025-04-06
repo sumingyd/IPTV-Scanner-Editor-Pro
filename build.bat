2cd93bf7ec1f1d4ff80447a4211e3f078a8138e2
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-04-07 00:38:20 +0800

    打包路径处理

--- build.bat
+++ build.bat
@@ -1,5 +1,17 @@
 @echo off
 chcp 65001 > nul
 set PYTHONUTF8=1
-pyinstaller --onefile --windowed --icon=logo.ico --add-data "icons/*;icons" --add-data "ffmpeg/*;ffmpeg" --add-data "vlc/*;vlc" --hidden-import python-vlc --hidden-import ffmpeg --name "IPTV扫描编辑工具" --noconfirm --clean main.py
+pyinstaller --onefile --windowed --icon=logo.ico ^
+--add-data "icons/*;icons" ^
+--add-data "ffmpeg/*;ffmpeg" ^
+--add-data "vlc/*;vlc" ^
+--add-data "vlc/plugins/*;vlc/plugins" ^
+--add-data "ffmpeg/bin/*;ffmpeg/bin" ^
+--add-data "ffmpeg/presets/*;ffmpeg/presets" ^
+--paths "C:\Users\sm\AppData\Local\Programs\Python\Python312\Lib\site-packages" ^
+--hidden-import python-vlc ^
+--hidden-import ffmpeg ^
+--runtime-hook=pyi_rth_vlc.py ^
+--name "IPTV扫描编辑工具" ^
+--noconfirm --clean main.py
 pause
