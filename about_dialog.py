8ffdfc73c25f7d048a3770ba785501304eff7aca
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-05-05 15:27:45 +0800

    更新版本

--- about_dialog.py
+++ about_dialog.py
@@ -12,7 +12,7 @@ logger = LogManager()
 
 class AboutDialog(QtWidgets.QDialog):
     # 版本配置
-    CURRENT_VERSION = "14.0.0.0"  # 当前版本号(手动修改这里)
+    CURRENT_VERSION = "15.0.0.0"  # 当前版本号(手动修改这里)
     DEFAULT_VERSION = None  # 将从GitHub获取最新版本
     BUILD_DATE = "2025-05-05"  # 更新为当前日期
     
