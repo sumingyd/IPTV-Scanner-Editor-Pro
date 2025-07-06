ba084171ccf2a2371484ff49a9ddf694d57a93fb
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-07-06 22:47:19 +0800

    up

--- 一键编译-无vlc+ffprobe.bat
+++ 一键编译-无vlc+ffprobe.bat
@@ -2,7 +2,6 @@
 chcp 65001 > nul
 set PYTHONUTF8=1
 pyinstaller --onefile --windowed --icon=logo.ico ^
---add-data "local_channel_mappings.txt;." ^
 --paths "C:\Users\sm\AppData\Local\Programs\Python\Python312\Lib\site-packages" ^
 --hidden-import python-vlc ^
 --hidden-import ffmpeg ^
