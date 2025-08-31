292430c42605c57e61d62288fa7f3beaba53338e
宿命 <45293856+sumingyd@users.noreply.github.com>
2025-08-31 16:34:27 +0800

    22.0 更新脚本

--- 一键编译-无vlc+ffprobe.bat
+++ 一键编译-无vlc+ffprobe.bat
@@ -2,6 +2,7 @@
 chcp 65001 > nul
 set PYTHONUTF8=1
 pyinstaller --onefile --windowed --icon=logo.ico ^
+--add-data "locales/*;locales" ^
 --paths "C:\Users\sm\AppData\Local\Programs\Python\Python312\Lib\site-packages" ^
 --hidden-import python-vlc ^
 --hidden-import ffmpeg ^
