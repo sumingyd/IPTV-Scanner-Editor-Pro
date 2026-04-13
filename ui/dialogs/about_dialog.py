8ceda15abee8578abaaa702fb792695d51160068
sumingyd <sumingyd@users.noreply.github.com>
2026-04-14 00:21:08 +0800

    fix(about_dialog): 更新当前版本号至44.0.0.3

--- ui/dialogs/about_dialog.py
+++ ui/dialogs/about_dialog.py
@@ -12,7 +12,7 @@ from ..floating_dialog import FloatingDialog
 
 
 class AboutDialog(FloatingDialog):
-    CURRENT_VERSION = "44.0.0.2"
+    CURRENT_VERSION = "44.0.0.3"
     DEFAULT_VERSION = None
     BUILD_DATE = "2026-04-13"
 
