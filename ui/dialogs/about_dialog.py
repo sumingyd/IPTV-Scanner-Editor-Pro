436d562ac1d37d4c7d5bea3bb6fba026b2ecfe0c
sumingyd <sumingyd@users.noreply.github.com>
2026-04-16 16:34:26 +0800

    chore: 更新关于对话框中的版本号和构建日期

--- ui/dialogs/about_dialog.py
+++ ui/dialogs/about_dialog.py
@@ -12,9 +12,9 @@ from ..floating_dialog import FloatingDialog
 
 
 class AboutDialog(FloatingDialog):
-    CURRENT_VERSION = "44.0.0.4"
+    CURRENT_VERSION = "44.0.0.5"
     DEFAULT_VERSION = None
-    BUILD_DATE = "2026-04-15"
+    BUILD_DATE = "2026-04-16"
 
     def __init__(self, parent=None):
         super().__init__(parent)
