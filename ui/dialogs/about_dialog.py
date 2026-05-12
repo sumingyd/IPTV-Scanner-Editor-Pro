553587dc7ea15321f5d5d61754d105435980da35
sumingyd <sumingyd@users.noreply.github.com>
2026-05-13 06:08:34 +0800

    chore: 更新版本号和构建日期
    将版本从46.0.0.6更新至46.0.1.0，构建日期更新为2026-05-13

--- ui/dialogs/about_dialog.py
+++ ui/dialogs/about_dialog.py
@@ -12,9 +12,9 @@ from ..floating_dialog import FloatingDialog
 
 
 class AboutDialog(FloatingDialog):
-    CURRENT_VERSION = "46.0.0.6"
+    CURRENT_VERSION = "46.0.1.0"
     DEFAULT_VERSION = None
-    BUILD_DATE = "2026-05-10"
+    BUILD_DATE = "2026-05-13"
 
     def __init__(self, parent=None):
         super().__init__(parent, stay_on_top=False)
